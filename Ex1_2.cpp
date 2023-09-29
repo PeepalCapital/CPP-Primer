67e6f0c51f6f7be7810ddd7337f1a8a161508af3
Deepak Venkatesh <50081353+PeepalCapital@users.noreply.github.com>
2023-09-29 11:39:28 -0700

    Add files via upload

---
+++ Ex1_2.cpp
@@ -0,0 +1,10 @@
+//naming convention used by G++ mingw is .cpp
+
+#include <iostream>
+
+int main()
+{
+    return -1;
+}
+
+// Output with return as -1: PS D:\Programming\C++\.vscode> cd "d:\Programming\C++\.vscode\" ; if ($?) { g++ Ex1_2.cpp -o Ex1_2 } ; if ($?) { .\Ex1_2 }
